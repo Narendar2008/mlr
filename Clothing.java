afe33305992cb96f6ee6d95548e1ef60c38afcf5
Narendar2008 <138804053+Narendar2008@users.noreply.github.com>
2025-01-29 09:47:24 +0530

    Add files via upload

---
+++ Clothing.java
@@ -0,0 +1,10 @@
+public class Clothing extends Product{
+    public Clothing(String name,double price){
+        super(name,price);
+    }
+    @Override
+    public void showCategory(){
+        System.out.println("Category: Clothing - "+getName()+", Price: $"+getPrice());
+    }
+    
+}
---
+++ Customer.java
@@ -0,0 +1,13 @@
+public class Customer extends User {
+    public Customer(String name,String email){
+        super(name,email);
+    }
+    @Override
+    public void perfromRole(){
+         System.out.println("Customer: Browsing and purchasing products.");
+
+    }
+    public void placeOder(Product product){
+        System.out.println("Customer: Placing order for "+product.getName());
+    }
+}
