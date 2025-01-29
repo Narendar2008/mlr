07a19f302af957362d570b897d5fcd78c96b936c
Narendar2008 <138804053+Narendar2008@users.noreply.github.com>
2025-01-29 11:41:02 +0530

    Delete Electronics.java

--- Electronics.java
+++
@@ -1,9 +0,0 @@
-public class Electronics extends Product{
-    public Electronics(String name,double price){
-        super(name,price);
-    }
-    @Override
-    public void showCategory(){
-        System.out.println("Category: Electronics - "+getName()+", Price: $"+getPrice());
-    }
-}
