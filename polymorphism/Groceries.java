1ff3e128d54571279fc7f65da49511e71ead5a21
Narendar2008 <138804053+Narendar2008@users.noreply.github.com>
2025-01-29 11:39:32 +0530

    Add files via upload

---
+++ polymorphism/Groceries.java
@@ -0,0 +1,10 @@
+public class Groceries extends Product {
+    public Groceries(String name, double price) {
+        super(name, price);
+    }
+
+    @Override
+    public void showCategory() {
+        System.out.println("Category: Groceries - " + getName() + ", Price: $" + getPrice());
+    }
+}
